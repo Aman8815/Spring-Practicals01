7fc602c36557e7c1a0393aae77b8a880baf93f7c
Aman8815 <amanyashona465@gmail.com>
2025-10-27 13:11:06 +0530

    done

--- Spring-Core/src/main/java/com/rays/TestPerson.java
+++ Spring-Core/src/main/java/com/rays/TestPerson.java
@@ -9,19 +9,25 @@ public class TestPerson {
 
 	public static void main(String[] args) {
 
-		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("employee.xml"));
+		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("multi.xml"));
 
 		Employee p = (Employee) factory.getBean("employee");
 
 		System.out.println("person's Empname: " + p.getEmpName());
 		System.out.println("person's salary: " + p.getSalary());
+				System.out.println("-----");
 		
-//		System.out.println("-----");
-//		
-//		Person p1 = (Person) factory.getBean("person1");
-//
-//		System.out.println("person's name: " + p1.getName());
-//		System.out.println("person's address: " + p1.getAddress());
+		Person p1 = (Person) factory.getBean("Person");
+		System.out.println("Person's id:" + p1.getId());
+		System.out.println("person's name: " + p1.getName());
+		System.out.println("person's address: " + p1.getSalary());
+		
+		System.out.println("------------");
+		
+		Doctor d =(Doctor) factory.getBean("Doctor");
+		System.out.println("Doctor's id :"+d.getId());
+		System.out.println("Doctor's name:"+d.getName());
+		System.out.println("Doctor's S[ecilities:"+d.getSpecilites());
 
 	}
 
