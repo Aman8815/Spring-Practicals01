d538516ffa32b6ec49d347694d1c71728f67266a
Aman8815 <amanyashona465@gmail.com>
2025-11-01 14:34:31 +0530

    mdone

--- Spring-Core/src/main/java/com/rays/collaction/MyBean1.java
+++ Spring-Core/src/main/java/com/rays/collaction/MyBean1.java
@@ -4,22 +4,29 @@ import java.util.Queue;
 import java.util.Set;
 
 public class MyBean1 {
-	
+
 	private Queue<String> queue;
 	private Set<String> set;
-   
-	
-	public MyBean1(Queue<String> queue ,Set<String> set) {
+
+	public Queue<String> getQueue() {
+		return queue;
+	}
+
+	public void setQueue(Queue<String> queue) {
 		this.queue = queue;
+	}
+
+	public Set<String> getSet() {
+		return set;
+	}
+
+	public void setSet(Set<String> set) {
 		this.set = set;
 	}
-	
 
-	
-	 
 	public void DisplayData() {
-		System.out.println("queue =" +queue);
-		System.out.println("SET"+set);
+		System.out.println("queue =" + queue);
+		System.out.println("SET" + set);
 	}
 
 }
